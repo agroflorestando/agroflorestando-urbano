5c084739d80fb9abb771d8e41e2b9a18721d76f8
Ricardo Azzi Silva <ricardoazzi91@hotmail.com>
2024-02-18 14:57:07 -0300

    definindo modelos de dados

--- src/app/pages/plant/plant.component.ts
+++ src/app/pages/plant/plant.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './plant.component.scss'
 })
 export class PlantComponent {
+  plant =     {
+    //  o nome virá do objeto que representa um
+    //  elemento concreto no mundo real
+    name: 'laranjeira do bairro',
+    //  imagem associada no registro original ou
+    //  então o primeiro comentário com imagem
+    image: '',
+    lists: [
+      {
+        name: 'laranjeiras',
+        author: 'pedrinho'
+      },
+      
+      {
+        name: 'laranjeiras belo horizonte',
+        author: 'joãozinho'
+      },
+      
+      {
+        name: 'citricas',
+        author: 'ananias'
+      },
+      
+      {
+        name: 'citrus sinensis',
+        author: 'agroflorestando'
+      }
+    ],
 
+  };
 }
